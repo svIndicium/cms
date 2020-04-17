66fcd3e2cb64601099e3c6fd66582d4baa9a9be4
rhettvankorlaar <rhettvankorlaar@gmail.com>
2020-04-17 20:52:17 +0200

    Added User package with initial user entity

---
+++ src/main/java/hu/indicium/cms/user/User.java
@@ -0,0 +1,22 @@
+package hu.indicium.cms.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private String email;
+    private int role;
+}
