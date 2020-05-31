be50fec648aa5abdd52c382735765c1666a0f9df
rhettvankorlaar <rhettvankorlaar@gmail.com>
2020-05-31 21:31:29 +0200

    Add Travis CI

--- src/main/java/hu/indicium/cms/user/UserRepository.java
+++ src/main/java/hu/indicium/cms/user/UserRepository.java
@@ -1,7 +1,9 @@
 package hu.indicium.cms.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     User findUserByEmail(String email);
 }
