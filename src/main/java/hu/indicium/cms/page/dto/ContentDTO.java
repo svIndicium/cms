23687968fa9d29bec8705ca0a1d77c61d9839f91
rhettvankorlaar <rhettvankorlaar@gmail.com>
2020-05-31 17:22:13 +0200

    Add request and dto

--- src/main/java/hu/indicium/cms/page/dto/ContentDTO.java
+++ src/main/java/hu/indicium/cms/page/dto/ContentDTO.java
@@ -1,7 +1,13 @@
 package hu.indicium.cms.page.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ContentDTO {
     private Long id;
     private String title;
     private String text;
+    private PageDTO pageDTO;
 }
